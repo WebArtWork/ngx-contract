aa772c44ec9825c321cbfdfbcdffa956b3eb5e89
ArturHoncharuk <dotwork.play@gmail.com>
2022-05-05 12:53:03 +0300

    service

--- contract.service.ts
+++ contract.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
-import { MongoService, AlertService } from 'wacom';
+import {Injectable} from '@angular/core';
+import {MongoService, AlertService, ModalService} from 'wacom';
+import { CreateComponent } from './create/create.component';
+import { ContractsComponent } from './contracts/contracts.component';
 
 @Injectable({
     providedIn: 'root'
@@ -7,41 +9,54 @@ import { MongoService, AlertService } from 'wacom';
 export class ContractService {
     public contracts: any = [];
     public _contracts: any = {};
-    public bySto:any ={}
-    constructor(private mongo: MongoService, private alert: AlertService) {
-        this.contracts = mongo.get('contract', {}, (arr, obj)=>{
+
+    new_contract(users, title) {
+        this.modal.show({component: CreateComponent, users, title});
+    }
+
+    open_contract(title) {
+        this.modal.show({component: ContractsComponent, title});
+    }
+
+    constructor(private mongo: MongoService, private alert: AlertService, public modal: ModalService) {
+        this.contracts = mongo.get('contract', {}, (arr, obj) => {
             this._contracts = obj;
         });
     }
-    create(contract:any={}, text = 'contract has been created.') {
-        if(contract._id) return this.save(contract);
-        this.mongo.create('contract', contract, created=>{
-            this.alert.show({ text });
+
+    create(contract: any = {}, text = 'contract has been created.') {
+        if (contract._id) { return this.save(contract); }
+        this.mongo.create('contract', contract, created => {
+            this.alert.show({text});
         });
     }
-    doc(contractId){
-        if(!this._contracts[contractId]){
+
+    doc(contractId) {
+        if (!this._contracts[contractId]) {
             this._contracts[contractId] = this.mongo.fetch('contract', {
                 query: {
-                    _id: contractId
+                    _id: contractId,
                 }
             });
         }
         return this._contracts[contractId];
     }
+
     update(contract, text = 'contract has been updated.') {
-        this.mongo.afterWhile(contract, _=> {
+        this.mongo.afterWhile(contract, _ => {
             this.save(contract, text);
         });
     }
-    save(contract, text = 'contract has been updated.'){
-        this.mongo.update('contract', contract, _=>{
-            if(text) this.alert.show({ text, unique: contract });
+
+    save(contract, text = 'contract has been updated.') {
+        this.mongo.update('contract', contract, _ => {
+            if (text) { this.alert.show({text, unique: contract}); }
         });
     }
+
     delete(contract, text = 'contract has been deleted.') {
-        this.mongo.delete('contract', contract, _=>{
-            if(text) this.alert.show({ text });
+        this.mongo.delete('contract', contract, _ => {
+            if (text) { this.alert.show({text}); }
         });
     }
 }
