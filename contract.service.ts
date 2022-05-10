73e8a583cbf69646766d9a4a4bedd7e42378fe67
Oganesman <96392987+qweRTyiOp123789@users.noreply.github.com>
2022-05-10 15:30:31 +0300

    Add files via upload

--- contract.service.ts
+++ contract.service.ts
@@ -1,62 +1,70 @@
-import {Injectable} from '@angular/core';
-import {MongoService, AlertService, ModalService} from 'wacom';
+import { Injectable } from '@angular/core';
+import { MongoService, AlertService, ModalService } from 'wacom';
 import { CreateComponent } from './create/create.component';
 import { ContractsComponent } from './contracts/contracts.component';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class ContractService {
-    public contracts: any = [];
-    public _contracts: any = {};
+	public contracts: any = [];
+	public _contracts: any = {};
 
-    new_contract(users, title) {
-        this.modal.show({component: CreateComponent, users, title});
-    }
+	new_contract(users, contract, title) {
+		this.modal.show({ component: CreateComponent, users, contract, title });
+	}
 
-    open_contract(title) {
-        this.modal.show({component: ContractsComponent, title});
-    }
+	open_contract(title) {
+		this.modal.show({ component: ContractsComponent, title });
+	}
 
-    constructor(private mongo: MongoService, private alert: AlertService, public modal: ModalService) {
-        this.contracts = mongo.get('contract', {}, (arr, obj) => {
-            this._contracts = obj;
-        });
-    }
+	constructor(private mongo: MongoService, private alert: AlertService, public modal: ModalService) {
+		this.contracts = mongo.get('contract', {
+			// groups: {
+			// 	place: {
+			// 		field: doc => { return doc.place },
+			// 		allow: doc => doc.status === 'Accepted'
+			// 	}
+			// }
+		}, (arr, obj) => {
+			this._contracts = obj;
+			console.log(this._contracts);
+		});
+	}
 
-    create(contract: any = {}, text = 'contract has been created.') {
-        if (contract._id) { return this.save(contract); }
-        this.mongo.create('contract', contract, created => {
-            this.alert.show({text});
-        });
-    }
+	create(contract: any = {}, text = 'contract has been created.') {
+		if (contract._id) { return this.save(contract); }
+		this.mongo.create('contract', contract, created => {
+			this.alert.show({ text });
+		});
+	}
 
-    doc(contractId) {
-        if (!this._contracts[contractId]) {
-            this._contracts[contractId] = this.mongo.fetch('contract', {
-                query: {
-                    _id: contractId,
-                }
-            });
-        }
-        return this._contracts[contractId];
-    }
+	doc(contractId) {
+		if (!this._contracts[contractId]) {
+			this._contracts[contractId] = this.mongo.fetch('contract', {
+				query: {
+					_id: contractId,
+				}
+			});
+		}
+		return this._contracts[contractId];
+	}
 
-    update(contract, text = 'contract has been updated.') {
-        this.mongo.afterWhile(contract, _ => {
-            this.save(contract, text);
-        });
-    }
+	update(contract, text = 'contract has been updated.') {
+		this.mongo.afterWhile(contract, _ => {
+			this.save(contract, text);
+		});
+	}
 
-    save(contract, text = 'contract has been updated.') {
-        this.mongo.update('contract', contract, _ => {
-            if (text) { this.alert.show({text, unique: contract}); }
-        });
-    }
+	save(contract, text = 'contract has been updated.') {
+		this.mongo.update('contract', contract, _ => {
+			if (text) { this.alert.show({ text, unique: contract }); }
+		});
+	}
 
-    delete(contract, text = 'contract has been deleted.') {
-        this.mongo.delete('contract', contract, _ => {
-            if (text) { this.alert.show({text}); }
-        });
-    }
+	delete(contract, text = 'contract has been deleted.') {
+		this.mongo.delete('contract', contract, _ => {
+			if (text) { this.alert.show({ text }); }
+		});
+	}
 }
